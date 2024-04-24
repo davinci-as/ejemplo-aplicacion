1260b92d815d674e234cd50fda19ec54e3551200
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2024-04-24 20:33:55 -0300

    feat: actulizar juego con la logica

--- app/src/main/java/ar/edu/davinci/ejemploaplicacion/MainActivity.java
+++ app/src/main/java/ar/edu/davinci/ejemploaplicacion/MainActivity.java
@@ -9,20 +9,33 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int numeroGanador = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        double aleatorio = Math.random() * 6 + 1;
+        this.numeroGanador = (int) aleatorio;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void tirarDato(View view) {
+    public String jugar() {
         double aleatorio = Math.random() * 6 + 1;
         int numeroDato = (int) aleatorio;
         Log.i("tirar-datos", "Ganaste" + numeroDato);
+        if(numeroDato == this.numeroGanador) {
+            return "Ganaste: salió " + numeroDato;
+        } else {
+            return "Perdiste: sacaste " + numeroDato
+                + " debería ser " + this.numeroGanador;
+        }
+    }
+
+    public void tirarDato(View view) {
         LinearLayout resultados = findViewById(R.id.resultados);
         TextView resultado = new TextView(this);
-        resultado.setText(R.string.text_win);
         resultados.addView(resultado);
+        String mensaje = jugar();
+        resultado.setText(mensaje);
     }
 }
